3b870acd357ccb87e66da9b6a6f44956b8c337c4
Geordi Feijens <geordi.feijens@student.kdg.be>
2019-08-19 23:32:19 +0200

    Changed name of app

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Research Geordi Feijens</title>
 
         <!-- Styles -->
         <link href="{{asset('css/app.css')}}" type="text/css" rel="stylesheet"/>
